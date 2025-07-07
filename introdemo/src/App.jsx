2c22fa7dd25769379f2274ba515a0901ed1f637f
Bonkers <boroboro1@interia.pl>
2025-07-07 12:29:28 +0200

    Component state, event handlers

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,15 +1,34 @@
+import { useState } from "react"
 
-const Hello = (props) => {  
-  console.log(props)
+const Hello = ({ name, age }) => {  
+
+  const bornYear = () => new Date().getFullYear() - age
+
   return (    
     <div>      
-      <p>Hello {props.name}, you are {props.age} years old</p>    
+      <p>Hello {name}, you are {age} years old</p>    
+      <p>So you were probably born in {bornYear()}</p>
     </div>  
+  )
+}
+
+export const Display = ({ counter }) => <div>{counter}</div>
+
+export const Button = ({ text, onClick }) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
   )
 }
 
 const App = () => {
   console.log('Hello from component')
+  const [counter, setCounter] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const setToZero = () => setCounter(0)
+  const decreaseByOne = () => setCounter(counter - 1)
 
   const age = 10
   const name = 'Peter'
@@ -18,6 +37,10 @@ const App = () => {
       <p>Hello world</p>
       <Hello name='George' age={26 + 10}/>
       <Hello name={name} age={age}/>
+      <Display counter={counter} />
+      <Button text='plus' onClick={increaseByOne} />
+      <Button text='zero' onClick={setToZero} />
+      <Button text='minus' onClick={decreaseByOne} />
     </div>
   )
 }

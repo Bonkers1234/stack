44a0e0a0d6eff7111b472b82d5c80842b5d60f77
Bonkers <boroboro1@interia.pl>
2025-07-30 15:55:58 +0200

    Added extra routes and errorHandler

--- part3/index.js
+++ part3/index.js
@@ -12,40 +12,31 @@ morgan.token('data', (request) => {
   }
 })
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if(error.name === 'CastError') {
+    return response.status(400).send({ error: 'Malformed ID' })
+  }
+
+  next(error)
+}
+
 app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
-let persons = [
-  { 
-    "id": "1",
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": "2",
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": "3",
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": "4",
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-
 app.get('/info', (request, response) => {
   const date = Date()
-  response.send(`
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${date}</p>
-    `)
+  Person.find({})
+    .then(persons => {
+      console.log(persons.length)
+      response.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${date}</p>
+        `)
+    })
 })
 
 app.get('/api/persons', (request, response) => {
@@ -54,33 +45,20 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  // const id = request.params.id
-  // const person = persons.find(p => p.id === id)
-
-  // if(!person) {
-  //   response.status(404).end()
-  // } else {
-  //   response.json(person)
-  // }
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+  .then(person => {
+      if(!person) {
+        response.status(404).end()
+      } else {
+        response.json(person)
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
-  // const isNameTaken = persons.find(p => p.name.toLowerCase() === body.name.toLowerCase())
-
-  // if(!body.name || !body.number) {
-  //   return response.status(400).json({
-  //     error: 'Name or Number is missing'
-  //   })
-  // } else if(isNameTaken) {
-  //   return response.status(400).json({
-  //     error: 'Name already in phonebook, name must be unique'
-  //   })
-  // }
 
   if(!body.name || !body.number) {
     return response.status(400).json({
@@ -98,14 +76,35 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(p => p.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))  
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
 
+  Person.findById(request.params.id)
+    .then(person => {
+
+      if(!person) {
+        return request.status(404).end()
+      }
+
+      person.number = number
+
+      person.save()
+        .then(updatedPerson => {
+          response.json(updatedPerson)
+        })
+    })
+    .catch(error => next(error))
+})
+
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT

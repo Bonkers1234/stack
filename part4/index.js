eb4a73afbc16fdd330505c3171cb2ee0045d0e49
Bonkers <boroboro1@interia.pl>
2025-08-03 16:07:40 +0200

    Part4 backend setup

---
+++ part4/index.js
@@ -0,0 +1,13 @@
+
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const app = require('./app')
+
+
+
+
+
+
+app.listen(config.PORT, () => {
+  logger.info('Server is running on ', config.PORT)
+})

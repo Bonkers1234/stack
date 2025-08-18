3ca2b9af779f17e9bb559f76308c17b62276f607
Bonkers <boroboro1@interia.pl>
2025-08-18 12:05:23 +0200

    Exercise 4.23 complete

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -4,14 +4,31 @@ const assert = require('node:assert')
 const mongoose = require('mongoose')
 const app = require('../app')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 const supertest = require('supertest')
 const helper = require('./test_helper')
 
 const api = supertest(app)
 
+const getToken = async () => {
+  const request = await api
+    .post('/api/login')
+    .send({
+      username: helper.initialUsers[0].username,
+      password: helper.initialUsers[0].password
+    })
+
+    return `Bearer ${request.body.token}`
+}
+
 beforeEach(async () => {
   await Blog.deleteMany({})
   await Blog.insertMany(helper.initialBlogs)
+
+  await User.deleteMany({})
+  await api
+    .post('/api/users')
+    .send(helper.initialUsers[0])
 })
 
 
@@ -42,6 +59,7 @@ test('successfully creates new blog', async () => {
 
   const response = await api
     .post('/api/blogs')
+    .set('Authorization', await getToken())
     .send(newBlog)
     .expect(201)
 
@@ -50,6 +68,28 @@ test('successfully creates new blog', async () => {
 
   const blogsAtEnd = await helper.blogsInDb()
   assert.strictEqual(blogsAtEnd.length, blogsAtStart.length + 1)
+})
+
+test('fails to create a blog with code 401 if token is missing', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+
+  const newBlog = {
+    title: "TEST",
+    author: "Robert",
+    url: "http://blog.com",
+    likes: 2,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(401)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, blogsAtStart.length)
+
+  const titles = blogsAtEnd.map(b => b.title)
+  assert(!titles.includes(newBlog.title))
 })
 
 test('missing likes default to 0', async () => {
@@ -61,6 +101,7 @@ test('missing likes default to 0', async () => {
 
   const response = await api
     .post('/api/blogs')
+    .set('Authorization', await getToken())
     .send(newBlog)
     .expect(201)
     .expect('Content-Type', /application\/json/)
@@ -76,22 +117,38 @@ test('missing Title or Url default to 400', async () => {
   
   await api
     .post('/api/blogs')
+    .set('Authorization', await getToken())
     .send(newBlog)
     .expect(400)
 })
 
 test('deleting a blog succeeds with status code 204 if id is valid', async () => {
   const blogsAtStart = await helper.blogsInDb()
-  const blogToDelete = blogsAtStart[0]
+  // const blogToDelete = blogsAtStart[0]
+  const blogToDelete = await api
+    .post('/api/blogs')
+    .set('Authorization', await getToken())
+    .send({
+      title: 'DELETE ME',
+      author: 'DELETE ME',
+      url: 'DELETE ME'
+    })
+    .expect(201)
 
-  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+  const blogsBeforeDelete = await helper.blogsInDb()
+  assert.strictEqual(blogsAtStart.length + 1, blogsBeforeDelete.length)
+
+  await api
+    .delete(`/api/blogs/${blogToDelete.body.id}`)
+    .set('Authorization', await getToken())
+    .expect(204)
 
   const blogsAtEnd = await helper.blogsInDb()
 
   const titles = blogsAtEnd.map(b => b.title)
-  assert(!titles.includes(blogToDelete.title))
+  assert(!titles.includes(blogToDelete.body.title))
 
-  assert.strictEqual(blogsAtStart.length - 1, blogsAtEnd.length)
+  assert.strictEqual(blogsAtStart.length, blogsAtEnd.length)
 })
 
 test('correctly updates a blog', async () => {

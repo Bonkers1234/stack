eb2764d1f679bca9b3d4077f9ccaf9085faf4d42
Bonkers <boroboro1@interia.pl>
2025-11-07 17:32:07 +0100

    Setup project directory for bloglist-redux

---
+++ part7/bloglist-redux/src/components/Notification.jsx
@@ -0,0 +1,13 @@
+
+const Notification = ({ info }) => {
+
+  if(!info) return null
+
+  return (
+    <div className={info.type}>
+      {info.text}
+    </div>
+  )
+}
+
+export default Notification

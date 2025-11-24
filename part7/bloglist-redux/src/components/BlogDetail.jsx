4134854004327d5143bdf8ac3ee27139b03d8e5f
Bonkers <boroboro1@interia.pl>
2025-11-24 12:15:51 +0100

    Setup for exercise 7.18

--- part7/bloglist-redux/src/components/BlogDetail.jsx
+++ part7/bloglist-redux/src/components/BlogDetail.jsx
@@ -14,17 +14,26 @@ const BlogDetail = ({ user, handleLikes, handleDelete }) => {
 
   return (
     <div>
-      <h2>{blog.title} - by {blog.author}</h2>
       <div>
-        {blog.url}
+        <h2>{blog.title} - by {blog.author}</h2>
+        <div>
+          {blog.url}
+        </div>
+        <div>
+          likes {blog.likes} <button onClick={() => handleLikes(blog)}>like</button>
+        </div>
+        <div>
+          {blog.user.name}
+        </div>
+        {canRemove && <button onClick={() => handleDelete(blog)}>delete</button>}
       </div>
       <div>
-        likes {blog.likes} <button onClick={() => handleLikes(blog)}>like</button>
+        <h3>Comments:</h3>
+        <ul>
+          <li>Test comment 1</li>
+          <li>Test comment 2</li>
+        </ul>
       </div>
-      <div>
-        {blog.user.name}
-      </div>
-      {canRemove && <button onClick={() => handleDelete(blog)}>delete</button>}
     </div>
   )
 }

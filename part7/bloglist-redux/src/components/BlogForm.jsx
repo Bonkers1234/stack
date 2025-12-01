fbc8f2130646b182422038e488fd693192ec8b07
Bonkers <boroboro1@interia.pl>
2025-12-01 11:53:00 +0100

    Styled BlogForm component

--- part7/bloglist-redux/src/components/BlogForm.jsx
+++ part7/bloglist-redux/src/components/BlogForm.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { addBlog } from '../reducers/blogsReducer'
 import { useNavigate } from 'react-router-dom'
+import { Form, Button } from 'react-bootstrap'
 
 const BlogForm = ({ blogFormRef }) => {
   const [title, setTitle] = useState('')
@@ -34,41 +35,55 @@ const BlogForm = ({ blogFormRef }) => {
   }
 
   return (
+    // <div>
+    //   <h2>Create new blog</h2>
+    //   <form onSubmit={handleSubmit}>
+    //     <div>
+    //       <label>
+    //         title
+    //         <input
+    //           type='text'
+    //           value={title}
+    //           onChange={({ target }) => setTitle(target.value)}
+    //         />
+    //       </label>
+    //     </div>
+    //     <div>
+    //       <label>
+    //         author
+    //         <input
+    //           type='text'
+    //           value={author}
+    //           onChange={({ target }) => setAuthor(target.value)}
+    //         />
+    //       </label>
+    //     </div>
+    //     <div>
+    //       <label>
+    //         url
+    //         <input
+    //           type='text'
+    //           value={url}
+    //           onChange={({ target }) => setUrl(target.value)}
+    //         />
+    //       </label>
+    //     </div>
+    //     <button type='submit'>create</button>
+    //   </form>
+    // </div>
     <div>
       <h2>Create new blog</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            title
-            <input
-              type='text'
-              value={title}
-              onChange={({ target }) => setTitle(target.value)}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            author
-            <input
-              type='text'
-              value={author}
-              onChange={({ target }) => setAuthor(target.value)}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            url
-            <input
-              type='text'
-              value={url}
-              onChange={({ target }) => setUrl(target.value)}
-            />
-          </label>
-        </div>
-        <button type='submit'>create</button>
-      </form>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className='w-25'>
+          <Form.Label>title:</Form.Label>
+          <Form.Control type='text' value={title} onChange={({ target }) => setTitle(target.value)} />
+          <Form.Label>author:</Form.Label>
+          <Form.Control type='text' value={author} onChange={({ target }) => setAuthor(target.value)} />
+          <Form.Label>url:</Form.Label>
+          <Form.Control type='text' value={url} onChange={({ target }) => setUrl(target.value)} />
+          <Button className='my-2' variant='primary' type='submit'>create</Button>
+        </Form.Group>
+      </Form>
     </div>
   )
 }

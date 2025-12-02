1a30f3fb01486ee5fed344ceef89697b120cd4d5
Bonkers <boroboro1@interia.pl>
2025-12-02 13:42:14 +0100

    Added responsive LoginForm styling

--- part7/bloglist-redux/src/components/LoginForm.jsx
+++ part7/bloglist-redux/src/components/LoginForm.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { setUser, logUser } from '../reducers/userReducer'
 import { useNavigate } from 'react-router-dom'
-import { Form, Button } from 'react-bootstrap'
+import { Form, Button, Container, Row, Col } from 'react-bootstrap'
 
 
 const LoginForm = () => {
@@ -31,16 +31,22 @@ const LoginForm = () => {
   }
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <h2>log in to application</h2>
-      <Form.Group className='w-25'>
-        <Form.Label>username:</Form.Label>
-        <Form.Control type='text' value={username} onChange={({ target }) => setUsername(target.value)} />
-        <Form.Label>password:</Form.Label>
-        <Form.Control type='password' value={password} onChange={({ target }) => setPassword(target.value)} />
-        <Button className='my-2' variant='primary' type='submit'>login</Button>
-      </Form.Group>
-    </Form>
+    <Container className='vh-100 d-flex align-items-center justify-content-center'>
+      <Row className='w-100 justify-content-center'>
+        <Col xs={12} md={6} lg={3}>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group>
+              <h2 className='text-center'>log in to application</h2>
+              <Form.Label>username:</Form.Label>
+              <Form.Control type='text' value={username} onChange={({ target }) => setUsername(target.value)} />
+              <Form.Label>password:</Form.Label>
+              <Form.Control type='password' value={password} onChange={({ target }) => setPassword(target.value)} />
+              <Button className='my-2' variant='primary' type='submit'>login</Button>
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
